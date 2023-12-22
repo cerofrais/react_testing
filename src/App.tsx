4d99cfb3af3baec9f42666e750638530555d3048
cerofrais <harsha.iitj08@gmail.com>
2023-12-22 16:58:36 +0530

    testing sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useSound from 'use-sound';
+
+import boopSfx from './boop.mp3';
+
 
 function App() {
+  const button = document.getElementById('btn');
+
+  const [play] = useSound(boopSfx);
+  button?.addEventListener('click', function handleClick(event) {
+    console.log('button clicked');
+    play();
+      });
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        
+          <button id="btn">Click</button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
